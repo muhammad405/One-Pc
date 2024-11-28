967eebe1b2c556280feccc400d1a8b1a89de0b91
Behruz <xoliqbediyevbehruz@gmail.com>
2024-11-28 16:44:16 +0500

    feat(common): write test for common apis

--- common/tests.py
+++ common/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from common import models
+
+
+class ContactUsTestCase(APITestCase):
+    def test_user_can_contact(self):
+        data = {
+            'name': 'Behruz',
+            'phone_number': '+998947099971',
+        }
+        response = self.client.post('/api/v1/common/contact-us/', data=data)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertIn('name', response.data)
+        self.assertIn('phone_number', response.data)
+
+
+class AboutUsTestCase(APITestCase):
+    def test_user_can_see_about_us(self):
+        models.AboutUs.objects.create(
+            title_uz='title_uz', title_ru='title_ru', title_en='title_en',
+            description_uz='description_uz', description_ru='description_ru', description_en='description_en',
+            video='test.mp4'
+        )
+        response = self.client.get('/api/v1/common/about-us/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('id', response.data[0])
+        self.assertIn('title_uz', response.data[0])
+        self.assertIn('title_ru', response.data[0])
+        self.assertIn('title_en', response.data[0])
+        self.assertIn('description_uz', response.data[0])
+        self.assertIn('description_ru', response.data[0])
+        self.assertIn('description_en', response.data[0])
+        self.assertIn('video', response.data[0])
+
+
+class AdvertisementTestCase(APITestCase):
+    def test_advertisement_list(self):
+        models.Advertisement.objects.create(
+            image='test.png',
+            link='test.com',
+        )
+        response = self.client.get('/api/v1/common/advertisement/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('id', response.data[0])
+        self.assertIn('image', response.data[0])
+        self.assertIn('link', response.data[0])
--- common/urls.py
+++ common/urls.py
@@ -4,7 +4,7 @@ from common import views
 
 
 urlpatterns = [
-    path('contact_us/', views.ContactUsApiView.as_view(), name='contact-us'),
+    path('contact-us/', views.ContactUsApiView.as_view(), name='contact-us'),
     path('about-us/', views.AboutUsListApiView.as_view(), name='about-us-videos'),
     path('advertisement/', views.AdvertisementApiView.as_view(), name='advertisement'),
 ]
