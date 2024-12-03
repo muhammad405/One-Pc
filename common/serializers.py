818084e556f0f342dfe6eb408a7d13170f8eb477
Behruz <xoliqbediyevbehruz@gmail.com>
2024-12-03 16:42:21 +0500

    fix image url

--- common/serializers.py
+++ common/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
 from common import models
-from core.settings import BASE_URL
 
 
 class ContactUsSerializer(serializers.ModelSerializer):
@@ -19,9 +18,6 @@ class AboutUsSerializer(serializers.ModelSerializer):
             'id', 'title_uz', 'title_ru', 'title_en', 'description_uz', 'description_ru', 'description_en', 'video'
         ]
 
-    def get_video(self, obj):
-        return f'{BASE_URL}{obj.video.url}'
-
 
 class AdvertisingSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField(method_name='get_image')
@@ -29,6 +25,3 @@ class AdvertisingSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Advertisement
         fields = ['id', 'image', 'link']
-
-    def get_image(self, obj):
-        return f'{BASE_URL}{obj.image.url}'
