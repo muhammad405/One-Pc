94b9c29b99473526b710489ad3dadfab777b7e0c
behruz-dev <xoliqbediyevbehruz@gmail.com>
2025-03-12 13:58:51 +0500

    change product admin

--- product/admin.py
+++ product/admin.py
@@ -52,7 +52,7 @@ class ProductAdmin(TranslationAdmin):
     list_display = ['item', 'name_uz', 'name_ru', 'name_en', 'category', 'main_image']
     list_editable = ['name_uz', 'name_ru', 'name_en', 'category', 'main_image']
     search_fields = ('item',)
-
+    list_filter = ['category'] 
 
 
 @admin.register(models.ProductCategory)
